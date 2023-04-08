8b8ddf936d8c26bdbc33bfa7daffb22fc6cee59a
lilka100 <107567575+lilka100@users.noreply.github.com>
2023-04-08 17:49:43 +0100

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,7 @@
+const btnmenu = document.querySelector('#btnmenu');
+const navbar = document.querySelector('.navbar');
+const header = document.querySelector('header')
+
+btnmenu.addEventListener('click', () => {
+   header.style = 'width : 200px';
+})
